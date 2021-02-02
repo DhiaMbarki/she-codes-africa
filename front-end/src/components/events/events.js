5ff61ea24f94e753a8364d169f384040cf4adba2
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-02-02 12:20:29 +0100

    events

---
+++ front-end/src/components/events/events.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import FakeEvents from './fakeEvents'
+
+ class Events extends Component {
+    constructor(propos){
+    super(propos);
+    this.state={
+        FakeEvents :FakeEvents
+    }
+    }
+    render() {
+        const eventItems = this.state.FakeEvents.map((event,index) => (
+            <div key={index}> 
+            <h3>{event.Title}</h3>
+            <h4>{event.Status}</h4>
+            <p>{event.Image}</p>
+            </div>
+        ))
+        return (
+            <div>
+                <h1>Events </h1>
+                {eventItems}
+            </div>
+        )
+    }
+}
+export default Events;
