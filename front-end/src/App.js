47abcf01086e8c5f31e67a726893e143e885bbe1
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-02-02 13:56:06 +0100

    fixing add.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Events from './components/events/events';
-import AddEvents from './components/events/addEvents';
 
 
 function App() {
   return (
     <div className="App">
         <h1>welcome</h1>
-    
         <Events />
     </div>
   );
