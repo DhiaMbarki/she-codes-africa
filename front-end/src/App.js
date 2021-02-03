37a52f595763ad7277bf57b8c7576b98a7ecf2da
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-02-03 13:59:30 +0100

    adddinfg redux

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import{Provider} from 'react-redux';
+import { Provider } from "react-redux";
 
-//IMPORTING ALL THE COMPONENTS 
-import Events from './components/events/events';
+//IMPORTING ALL THE COMPONENTS
+import Events from "./components/events/events";
 
-import './App.css';
+import store from './redux/store'
+
+import "./App.css";
+
 
 function App() {
   return (
-    <Provider store ={store}>
-    <BrowserRouter>
-    <div className="App">
-        
-    <Switch>
-    {/* <Route exact path="/" component={Homepage} />
-    <Route exact path="/Homepage" component={Homepage} /> */}
-    <Route exact path="/Events" component={Events} />
-     
-    </Switch>
-    </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/Events" component={Events} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </Provider>
   );
 }
