0fe05b4c5a241010daf28b9d97b4a4d7e48ccc39
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-02-03 15:05:40 +0100

    implement redux in event section

---
+++ front-end/src/redux/reducers/eventReducer.js
@@ -0,0 +1,14 @@
+import { FETCH_EVENTS} from "../actions/eventTypes";
+
+const initialState = {
+  events: [],
+  event: {},
+};
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+   default : 
+  return state
+
+  }
+}
