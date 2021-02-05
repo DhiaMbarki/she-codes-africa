d782e1515d227d10448cc4bf48f9c448b7a108a8
Sabri0o <sabritrabelsi90@gmail.com>
2021-02-05 10:41:26 +0100

    adding card blogs

---
+++ front-end/src/components/upcomingEvents.js
@@ -0,0 +1,15 @@
+// UpcomingEvents.js
+
+import React, { Component } from 'react';
+
+class UpcomingEvents extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Upcoming Events</h2>
+        </div>
+    );
+  }
+}
+
+export default UpcomingEvents;
