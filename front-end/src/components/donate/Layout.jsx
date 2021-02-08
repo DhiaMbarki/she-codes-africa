58882e5adbba4c0e04b6e3aea9860a48a83799e9
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-02-08 14:36:31 +0100

    fixing color

--- front-end/src/components/donate/Layout.jsx
+++ front-end/src/components/donate/Layout.jsx
@@ -11,13 +11,15 @@ const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
 const Layout = ({ children, title }) => {
   return (
     <>
+    <h1>hi</h1>
+    <div>
       <GlobalStyles />
-      
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       
       <Elements stripe={stripePromise}>{children}</Elements>
+      </div>
     </>
   );
 };
--- front-end/src/components/donate/prebuilt/DonutShop.jsx
+++ front-end/src/components/donate/prebuilt/DonutShop.jsx
@@ -10,7 +10,7 @@ const Shop = styled.div`
 
 const ShopName = styled.h1`
   font-size: 18px;
-  color: #fff;
+
   font-style: normal;
   font-variant: normal;
   font-weight: 400;
