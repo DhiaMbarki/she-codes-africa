e05ad84ef70387d3e81403772a2aca1be0df51b2
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-02-08 14:42:25 +0100

    fixing color

--- front-end/src/components/donate/Layout.jsx
+++ front-end/src/components/donate/Layout.jsx
@@ -3,7 +3,6 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 
-
 const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
 
 
@@ -11,15 +10,14 @@ const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
 const Layout = ({ children, title }) => {
   return (
     <>
-    <h1>hi</h1>
-    <div>
+    <h1>Donation</h1>
+
       <GlobalStyles />
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       
       <Elements stripe={stripePromise}>{children}</Elements>
-      </div>
     </>
   );
 };
