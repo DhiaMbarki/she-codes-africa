4d3c1c1f26d0016b45805c0c202d63d9d28e20e4
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-02-11 17:37:09 +0100

    committing ghaston work

--- front-end/src/redux/actions/editprofileaction.js
+++ front-end/src/redux/actions/editprofileaction.js
@@ -1,14 +1,14 @@
-import {ADD_PROFILE}   from './eventTypes'
- const addprofileaction = ({ PhotosprofileUrl ,Firstname, Biography,Githubacountlink ,Linkedincountlink,Websiteurl}) => ({
-    type: ADD_PROFILE,
-    payload: {
-        PhotosprofileUrl,
-        Firstname,
-        Biography,
-        Githubacountlink,
-        Linkedincountlink,
-        Websiteurl
-    },
-  });
+// import {ADD_PROFILE}   from './eventTypes'
+//  const addprofileaction = ({ PhotosprofileUrl ,Firstname, Biography,Githubacountlink ,Linkedincountlink,Websiteurl}) => ({
+//     type: ADD_PROFILE,
+//     payload: {
+//         PhotosprofileUrl,
+//         Firstname,
+//         Biography,
+//         Githubacountlink,
+//         Linkedincountlink,
+//         Websiteurl
+//     },
+//   });
   
-  export default addprofileaction
+//   export default addprofileaction
