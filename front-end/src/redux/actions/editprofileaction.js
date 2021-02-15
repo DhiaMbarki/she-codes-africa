410a70aed72bffc9aa59c05c5f09678c0a77e880
Sabri0o <sabritrabelsi90@gmail.com>
2021-02-15 10:55:53 +0100

    fixing edit profile

--- front-end/src/redux/actions/editprofileaction.js
+++ front-end/src/redux/actions/editprofileaction.js
@@ -1,14 +1,14 @@
- import {ADD_PROFILE}   from './eventTypes'
- const addprofileaction = ( PhotosprofileUrl ,Firstname, Biography,Githubacountlink ,Linkedincountlink,Websiteurl) => ({
-    type: ADD_PROFILE,
-    payload: {
-        PhotosprofileUrl,
-        Firstname,
-        Biography,
-        Githubacountlink,
-        Linkedincountlink,
-        Websiteurl
-    },
-  });
-  
-  export default addprofileaction
+import {ADD_PROFILE}   from './eventTypes'
+const addprofileaction = ( PhotosprofileUrl ,Firstname, Biography,Githubacountlink ,Linkedincountlink,Websiteurl) => ({
+   type: ADD_PROFILE,
+   payload: {
+       PhotosprofileUrl,
+       Firstname,
+       Biography,
+       Githubacountlink,
+       Linkedincountlink,
+       Websiteurl
+   },
+ });
+ 
+ export default addprofileaction
