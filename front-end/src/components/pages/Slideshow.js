2274fc8b779955cec47bf7fa4f3a9c4d2728f498
DhiaMbarki <tahridhia22@gmail.com>
2021-02-16 00:57:05 +0100

    Refactor

--- front-end/src/components/pages/Slideshow.js
+++ front-end/src/components/pages/Slideshow.js
@@ -1,150 +1,150 @@
 // bad animation handling. timing is 2x too long
 import React from "react";
-import './Slideshow.scss'
-
+import "./Slideshow.scss";
 
 class Slideshow extends React.Component {
-    constructor() {
-      super();
-  
-      this.state = {
-        loaded: false,
-        animating: false,
-        animationDirection: "",
-        animationDuration: 300,
-        currentSlide: 0,
-        slides: [
-          {
-            title: "Raika",
-            imageUrl:
-              "https://www.pngkey.com/png/detail/381-3815262_our-vision-business-woman-black-png.png",
-            description: "An incredible plant to beautify your living room.",
-            details: {
-              temperature: "70 degrees F day 65 degrees F night",
-              water: "Summer: 2 litres Winter: 1 litre",
-              nutrition: "Garden loam, perlite, peat moss"
-            }
+  constructor() {
+    super();
+
+    this.state = {
+      loaded: false,
+      animating: false,
+      animationDirection: "",
+      animationDuration: 300,
+      currentSlide: 0,
+      slides: [
+        {
+          title: "Raika",
+          imageUrl:
+            "https://www.pngkey.com/png/detail/381-3815262_our-vision-business-woman-black-png.png",
+          description: "An incredible plant to beautify your living room.",
+          details: {
+            temperature: "70 degrees F day 65 degrees F night",
+            water: "Summer: 2 litres Winter: 1 litre",
+            nutrition: "Garden loam, perlite, peat moss",
           },
-          {
-            title: "Another Plant",
-            imageUrl:
-              "https://s3-us-west-2.amazonaws.com/s.cdpn.io/36124/plant2.png",
-            description: "This is another nice plant.",
-            details: {
-              temperature: "75 degrees F day 62 degrees F night",
-              water: "Summer: 3 litres Winter: 1.5 litre",
-              nutrition: "A thing, something, other thing"
-            }
-          }
-        ]
-      };
-  
-      this.changeSlide = this.changeSlide.bind(this);
-    }
-  
-    fireAnims(duration) {
+        },
+        {
+          title: "Another Plant",
+          imageUrl:
+            "https://dwkujuq9vpuly.cloudfront.net/news/wp-content/uploads/2018/09/iStock-840243874-960x480.jpg",
+          description: "This is another nice plant.",
+          details: {
+            temperature: "75 degrees F day 62 degrees F night",
+            water: "Summer: 3 litres Winter: 1.5 litre",
+            nutrition: "A thing, something, other thing",
+          },
+        },
+      ],
+    };
+
+    this.changeSlide = this.changeSlide.bind(this);
+  }
+
+  fireAnims(duration) {
+    this.setState({
+      animating: true,
+      animationDirection: "out",
+    });
+    // halfway
+    setTimeout(() => {
       this.setState({
         animating: true,
-        animationDirection: "out"
+        animationDirection: "in",
       });
-      // halfway
-      setTimeout(() => {
-        this.setState({
-          animating: true,
-          animationDirection: "in"
-        });
-      }, duration / 2);
-      // done
-      setTimeout(() => {
-        this.setState({
-          animating: false,
-          animationDirection: ""
-        });
-      }, duration);
-    }
-  
-    changeSlide(dir) {
-      const currentSlide = this.state.currentSlide;
-      const slides = this.state.slides;
-  
-      if (dir === "right") {
-        if (currentSlide < slides.length - 1) {
-          this.fireAnims(this.state.animationDuration * 2);
-          window.setTimeout(() => {
-            this.setState({
-              currentSlide: currentSlide + 1
-            });
-          }, this.state.animationDuration);
-        }
-      } else {
-        if (currentSlide > 0) {
-          this.fireAnims(this.state.animationDuration * 2);
-          window.setTimeout(() => {
-            this.setState({
-              currentSlide: currentSlide - 1
-            });
-          }, this.state.animationDuration);
-        }
-      }
-    }
-  
-    determineDir(delta) {
-      if (delta > 0) {
-        return "right";
-      } else {
-        return "left";
-      }
-    }
-  
-    componentDidMount() {
+    }, duration / 2);
+    // done
+    setTimeout(() => {
       this.setState({
-        loaded: true
+        animating: false,
+        animationDirection: "",
       });
-    }
-    render() {
-      let classes = ["slideshow"];
-      if (this.state.animating) {
-        classes.push(
-          "slideshow--animated slideshow--" + this.state.animationDirection
-        );
-      } else {
-        classes = ["slideshow"];
+    }, duration);
+  }
+
+  changeSlide(dir) {
+    const currentSlide = this.state.currentSlide;
+    const slides = this.state.slides;
+
+    if (dir === "right") {
+      if (currentSlide < slides.length - 1) {
+        this.fireAnims(this.state.animationDuration * 2);
+        window.setTimeout(() => {
+          this.setState({
+            currentSlide: currentSlide + 1,
+          });
+        }, this.state.animationDuration);
       }
-      return (
-        <div className={classes.join(" ")}>
-          <Slide
-            title={this.state.slides[this.state.currentSlide].title}
-            image={this.state.slides[this.state.currentSlide].imageUrl}
-            description={this.state.slides[this.state.currentSlide].description}
-            details={this.state.slides[this.state.currentSlide].details}
-            count={this.state.currentSlide + 1}
-            changeSlide={this.changeSlide}
-            slideLength={this.state.slides.length}
-          />
-        </div>
-      );
+    } else {
+      if (currentSlide > 0) {
+        this.fireAnims(this.state.animationDuration * 2);
+        window.setTimeout(() => {
+          this.setState({
+            currentSlide: currentSlide - 1,
+          });
+        }, this.state.animationDuration);
+      }
     }
   }
-  
-  class Slide extends React.Component {
-    constructor() {
-      super();
-  
-      this.state = {};
-    }
-    render() {
-      return (
-        <section id="projects">
 
+  determineDir(delta) {
+    if (delta > 0) {
+      return "right";
+    } else {
+      return "left";
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      loaded: true,
+    });
+  }
+  render() {
+    let classes = ["slideshow"];
+    if (this.state.animating) {
+      classes.push(
+        "slideshow--animated slideshow--" + this.state.animationDirection
+      );
+    } else {
+      classes = ["slideshow"];
+    }
+    return (
+      <div className={classes.join(" ")}>
+        <Slide
+          title={this.state.slides[this.state.currentSlide].title}
+          image={this.state.slides[this.state.currentSlide].imageUrl}
+          description={this.state.slides[this.state.currentSlide].description}
+          details={this.state.slides[this.state.currentSlide].details}
+          count={this.state.currentSlide + 1}
+          changeSlide={this.changeSlide}
+          slideLength={this.state.slides.length}
+        />
+      </div>
+    );
+  }
+}
+
+class Slide extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <section id="projects">
         <div className="slide">
           <div className="slide__decorative-sidebar">
             <img src={this.props.image} />
           </div>
-  
+
           <div className="slide__info">
             <div className="slide__info__text">
               <h1 className="slide__info__title">{this.props.title}</h1>
-              <p className="slide__info__description">{this.props.description}</p>
+              <p className="slide__info__description">
+                {this.props.description}
+              </p>
             </div>
             <img
               src={this.props.image}
@@ -158,7 +158,7 @@ class Slideshow extends React.Component {
                     ? `slide__arrows__arrow`
                     : `slide__arrows__arrow slide__arrows__arrow--disabled`
                 }
-                onClick={e => this.props.changeSlide("left")}
+                onClick={(e) => this.props.changeSlide("left")}
               >
                 {`<`}
                 {/* shrug */}
@@ -169,34 +169,32 @@ class Slideshow extends React.Component {
                     ? `slide__arrows__arrow`
                     : `slide__arrows__arrow slide__arrows__arrow--disabled`
                 }
-                onClick={e => this.props.changeSlide("right")}
-              >
-                
-              </a>
+                onClick={(e) => this.props.changeSlide("right")}
+              ></a>
             </div>
           </div>
-  
+
           <div className="slide__next">
             <span>Next: Factors</span>
           </div>
-  
+
           <div className="slide__details">
             <div className="slide__details__title">Discover the details</div>
-  
+
             <div className="slide__details__block slide__details__block--temp">
               <h3 className="slide__details__subtitle">Temperature</h3>
               <p className="slide__details__block__description">
                 {this.props.details.temperature}
               </p>
             </div>
-  
+
             <div className="slide__details__block slide__details__block--water">
               <h3 className="slide__details__subtitle">Water</h3>
               <p className="slide__details__block__description">
                 {this.props.details.water}
               </p>
             </div>
-  
+
             <div className="slide__details__block slide__details__block--nutrition">
               <h3 className="slide__details__subtitle">Nutrition</h3>
               <p className="slide__details__block__description">
@@ -204,7 +202,7 @@ class Slideshow extends React.Component {
               </p>
             </div>
           </div>
-  
+
           <div className="slide__count">
             <p className="slide__count__title">Explore</p>
             <span className="slide__count__count">
@@ -212,11 +210,11 @@ class Slideshow extends React.Component {
             </span>
           </div>
         </div>
-        </section>
-      );
-    }
+      </section>
+    );
   }
-  
-  // RENDER
-  
-export default Slideshow  
+}
+
+// RENDER
+
+export default Slideshow;
