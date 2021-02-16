419a62485b26e73dc6970554044a02104789ee97
Ghassen21 <ghassenayd@gmail.com>
2021-02-16 12:58:46 +0100

    fixing event and blogs front and back

--- front-end/src/redux/root_Reducer.js
+++ front-end/src/redux/root_Reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import eventsReducer from "./reducers/eventsReducer";
 import authReducer from "./reducers/authReducer";
-import editprofileReducer from "./reducers/editprofileReducer";
+import editprofileReducer from "./reducers/editProfileReducer";
 import blogReducer from "./reducers/blogsReducer";
 import addressourcesreducers from "./reducers/addressourcesreducers";
 
