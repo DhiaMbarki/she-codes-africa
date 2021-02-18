2a907bf8db7675094e7aa80711ac858b851b5054
NeirouzJbira <jbira.neirouz@gmail.com>
2021-02-18 09:41:20 +0100

    commenting the database side cz i dont have postgress

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
+const http = require("http");
 const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const { db } = require("./models/indexdb");
-const app = express();
 
 // const memerout = require("./routes/membersroute");
 // const eventrouter = require("./routes/eventsroute");
@@ -38,10 +44,55 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // set port, listen for requests
 
-////////////////////////////////////////////////////////  Webinar 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(
-    `listening on port ${PORT} and To get started, visit: http://localhost:3000`
+    `listening on port ${PORT}`
   );
+});
+////////////////////////////////////////////////////////  Webinar
+const users = {};
+
+const socketToRoom = {};
+
+io.on("connection", (socket) => {
+  socket.on("join room", (roomID) => {
+    if (users[roomID]) {
+      const length = users[roomID].length;
+      if (length === 4) {
+        socket.emit("room full");
+        return;
+      }
+      users[roomID].push(socket.id);
+    } else {
+      users[roomID] = [socket.id];
+    }
+    socketToRoom[socket.id] = roomID;
+    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+
+    socket.emit("all users", usersInThisRoom);
+  });
+
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((id) => id !== socket.id);
+      users[roomID] = room;
+    }
+  });
 });
